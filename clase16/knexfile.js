8271fae1e8ad6868e621e06df3c1151901cdf5f2
jaguilar992 <jaguilar992@gmail.com>
2022-08-18 18:10:32 -0600

    sqlite

--- clase16/knexfile.js
+++ clase16/knexfile.js
@@ -1,21 +1,17 @@
-const dotenv = require("dotenv")
-dotenv.config();
+// const dotenv = require("dotenv")
+// dotenv.config();
 
-const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
-const DATABASE_PORT = process.env.DATABASE_PORT || "3306";
-const DATABASE_USER = process.env.DATABASE_USER || "antonio";
-const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "password_1234";
-const DATABASE_NAME = process.env.DATABASE_NAME || "plataforma_db";
+// const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
+// const DATABASE_PORT = process.env.DATABASE_PORT || "3306";
+// const DATABASE_USER = process.env.DATABASE_USER || "antonio";
+// const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "password_1234";
+// const DATABASE_NAME = process.env.DATABASE_NAME || "plataforma_db";
 
 // console.log(DATABASE_HOST, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD);
 const knexConfig = {
-  client: 'mysql',
+  client: 'sqlite3',
   connection: {
-    host: DATABASE_HOST,
-    port: DATABASE_PORT,
-    user: DATABASE_USER,
-    password: DATABASE_PASSWORD,
-    database: DATABASE_NAME,
+    filename: 'database2.sqlite'
   },
   migrations: {
     tableName: 'knex_migrations',
