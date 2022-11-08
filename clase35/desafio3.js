63565e439e75add3a7fc8f7d76fd21891b721eb6
jaguilar992 <jaguilar992@gmail.com>
2022-11-08 13:14:49 -0600

    clase35

--- clase35/desafio3.js
+++ clase35/desafio3.js
@@ -3,15 +3,17 @@ const MY_PHONE = process.env.MY_PHONE;
 
 const sid = "AC7ff1d3d2460c6a05c0147a5b5b2cf72d";
 const token = "c9b2b02eb24f584dda2af2facebc19cf";
-
-
 const twilio = require("twilio");
 const client = twilio(sid, token);
 
+const argv = process.argv.slice(2);
+const PHONE_NUMBER = argv[0];
+const MESSAGE = argv[1];
+
 const smsOptions = {
   from: "+13609941323",
-  to: "MY_PHONE", // +504XXXXXXXX
-  body: "Hola desde Twilio & NodeJS - Mensaje2"
+  to: PHONE_NUMBER, // +504XXXXXXXX
+  body: MESSAGE
 }
 
 async function send() {
@@ -24,3 +26,5 @@ async function send() {
 }
 
 send();
+
+// node desafio3.js +50433332220 "Hola desde la terminal"
