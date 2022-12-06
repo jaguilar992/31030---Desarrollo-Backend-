bde400fbb4a57bb1756b7da6f15883d4df56a0b9
antonio-iconic <antonio-iconic@helloiconic.com>
2022-12-06 16:59:28 -0600

    Clase 44

---
+++ clase44/contenedor.js
@@ -0,0 +1,62 @@
+import fs from "fs";
+const encoding = "utf-8";
+
+export default class ContenedorArchivo {
+  constructor(path) {
+    this.filePath = path;
+    this.createFileIfNotExists();
+    const data = fs.readFileSync(this.filePath, encoding);
+    this.contenedor = JSON.parse(data);
+  }
+
+  createFileIfNotExists() {
+    if (!fs.existsSync(this.filePath)) {
+      fs.writeFileSync(this.filePath, "[]");
+    }
+  }
+
+  _saveAll(data) {
+    const stringData = JSON.stringify(data, null, 2);
+    fs.writeFileSync(this.filePath, stringData, encoding);
+  }
+
+  save(object) {
+    if (!object.id) {
+      const lastId = this.contenedor.reduce(
+        (acc, el) => (el.id > acc ? el.id : acc),
+        0
+      ); // Acumulador inicial);
+      const newId = lastId + 1;
+      object.id = newId;
+    }
+    this.contenedor.push(object);
+    this._saveAll(this.contenedor);
+    return object.id;
+  }
+
+  getById(id) {
+    return this.contenedor.find((c) => c.id === id);
+  }
+
+  getAll() {
+    return this.contenedor;
+  }
+
+  deleteById(id) {
+    const filtered = this.contenedor.filter((el) => el.id !== id);
+    this.contenedor = filtered;
+    this._saveAll(filtered);
+  }
+
+  deleteAll() {
+    this.contenedor = [];
+    this._saveAll([]);
+  }
+
+  updateById(id, object) {
+    const index = this.contenedor.findIndex((el) => el.id === id);
+    this.contenedor[index] = object;
+    this._saveAll(this.contenedor);
+    return this.contenedor[index];
+  }
+}
---
+++ clase44/server.js
@@ -0,0 +1,116 @@
+import express from "express";
+const app = express();
+
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+import crypto from "crypto";
+
+const schema = buildSchema(`
+  type Trainer {
+    id: ID!
+    name: String
+    city: String
+  }
+
+  input TrainerInput {
+    name: String
+    city: String
+  }
+
+  type Query {
+    getTrainer(id: ID!): Trainer
+    getTrainers(campo: String, valor: String): [Trainer]
+  }
+
+  type Mutation {
+    createTrainer(datos: TrainerInput): Trainer
+    updateTrainer(id: ID!, datos: TrainerInput): Trainer
+    deleteTrainer(id: ID!): Trainer
+  }
+`);
+
+class Trainer {
+  constructor (id, {name, city}) {
+    this.id = id;
+    this.name = name;
+    this.city = city;
+  }
+}
+
+const trainersContainer = {};
+/**
+ *  {
+ *    "1" : {name, id, city},
+ *    "2" : {name2, id2, city2},
+ *  }
+ * 
+ * */
+
+
+// Resolutions
+function getTrainer({ id }) {
+   if (!trainersContainer[id]) {
+    throw new Error('Trainer not found');
+   }
+   return trainersContainer[id];
+}
+
+/** 
+ * [
+ * {name, id, city},
+ * {name2, id2, city2},
+ * ]
+ * 
+ * 
+*/
+
+/***{ city:'Barcelona'}*/
+function getTrainers({ campo, valor }) {
+  const trainers = Object.values(trainersContainer);
+  if (campo && valor) {
+    return trainers.filter( p => p[campo] === valor);
+  } else return trainers;
+}
+
+function createTrainer({datos}) {
+  const id = crypto.randomBytes(10).toString('hex');
+  const newTrainer = new Trainer(id, datos);
+  trainersContainer[id] = newTrainer;
+  return newTrainer;
+}
+
+function updateTrainer ({id, datos}) {
+  if (!trainersContainer[id]) {
+    throw new Error('Trainer not found')
+  }
+  const updatedTrainer = new Trainer(id, datos);
+  trainersContainer[id] = updateTrainer;
+  return updatedTrainer
+}
+
+function deleteTrainer({id}) {
+  if (!trainersContainer[id]) {
+    throw new Error('Trainer not found')
+  }
+  const deletedTrainer = trainersContainer[id];
+  delete trainersContainer[id];
+  return deletedTrainer;
+}
+
+app.use("/graphql", graphqlHTTP({
+  schema,
+  rootValue: {
+    getTrainer,
+    getTrainers,
+    createTrainer,
+    updateTrainer,
+    deleteTrainer
+  },
+  graphiql: true,
+}));
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Ejecuntando servidor en puerto: ${PORT}`);
+});
